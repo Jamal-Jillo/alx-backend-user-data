5a970dea827eb6ae6f30570961b2eb18c4352f71
Jamal <jayjamal86@gmail.com>
2023-05-10 10:05:49 +0300

    more fixes

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,11 +2,11 @@
 """DB module
 """
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tuple_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound, MultipleResultsFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -45,13 +45,19 @@ class DB:
             new_user = None
         return new_user
 
-    def find_user_by(self, **kwargs):
+    def find_user_by(self, **kwargs) -> User:
         """Method that finds a user in the database by keyword arguments"""
 
-        try:
-            user = self.session.query(User).filter_by(**kwargs).one()
-            return user
-        except NoResultFound:
-            raise NoResultFound("No user found with the given query parameters.")
-        except MultipleResultsFound:
-            raise InvalidRequestError("Multiple users found with the given query parameters.")
+        fields, values = [], []
+        for key, value in kwargs.items():
+            if hasattr(User, key):
+                fields.append(getattr(User, key))
+                values.append(value)
+            else:
+                raise InvalidRequestError()
+        result = self._session.query(User).filter(
+            tuple_(*fields).in_([tuple(values)])
+        ).first()
+        if result is None:
+            raise NoResultFound()
+        return result
